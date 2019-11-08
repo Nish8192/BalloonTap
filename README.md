db8d9ed0741dfaada073a20313940ab58a57a858
nish8192 <nish8192@gmail.com>
2019-11-08 15:54:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+First Lua and Corona Project
---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local background = display.newImageRect("background.png", 360, 570)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+local platform = display.newImageRect("platform.png", 300, 50)
+platform.x = display.contentCenterX
+platform.y = display.contentHeight - 25
